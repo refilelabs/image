0d2db6fd6f2ae4307c2c384038793a66a3f419e6
Tom Voet <tomvoet@pm.me>
2026-03-08 14:01:26 +0100

    fix(Converter): remove self-to-self conversions from `possibleImageConversions`

--- composables/useFormats.ts
+++ composables/useFormats.ts
@@ -3,7 +3,9 @@ import { supportedFormats } from '#image/utils/file_types'
 export const useImageFormats = () => {
   const imageSourceFormats = supportedFormats.filter(format => format.decoding !== 'No').map(format => format.format)
   const imageTargetFormats = supportedFormats.filter(format => format.encoding !== 'No').map(format => format.format)
-  const possibleImageConversions = imageSourceFormats.flatMap(sourceFormat => imageTargetFormats.map(targetFormat => ({ sourceFormat, targetFormat })))
+  const possibleImageConversions = imageSourceFormats.flatMap((sourceFormat) => {
+    return imageTargetFormats.filter(targetFormat => targetFormat !== sourceFormat).map(targetFormat => ({ sourceFormat, targetFormat }))
+  })
   const allFormats = supportedFormats.map(format => format.format)
 
   return {
