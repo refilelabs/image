424667e595a71bb273a4e3d84700b61ee3f69a5c
Tom Voet <tomvoet@pm.me>
2026-03-14 23:55:48 +0100

    fix: buttongroup rounded borders

--- components/CompressionSettings.vue
+++ components/CompressionSettings.vue
@@ -26,30 +26,28 @@ const imageOptions = imageTypes.map(type => ({
 </script>
 
 <template>
-  <div>
-    <UPopover
-      :content="{
-        align: 'end',
-        side: 'bottom',
-        sideOffset: 8,
-      }"
-    >
-      <UButton label="Compression Settings" color="neutral" variant="subtle" icon="heroicons:cog-6-tooth" />
-      <template #content>
-        <div class="p-3 min-w-30 sm:min-w-40 md:min-w-60 space-y-4">
-          <UFormField label="Image Type">
-            <URadioGroup v-model="imageType" :items="imageOptions" />
-          </UFormField>
-          <UFormField
-            label="Quality" :ui="{
-              container: 'flex flex-col md:flex-row md:items-center mt-0 md:mt-1',
-            }"
-          >
-            <span class="mb-1 md:mb-0 md:mr-2">{{ (quality * 100).toFixed(0) }}%</span>
-            <USlider v-model="quality" :min="0" :max="1" :step="0.01" />
-          </UFormField>
-        </div>
-      </template>
-    </UPopover>
-  </div>
+  <UPopover
+    :content="{
+      align: 'end',
+      side: 'bottom',
+      sideOffset: 8,
+    }"
+  >
+    <UButton label="Compression Settings" color="neutral" variant="subtle" icon="heroicons:cog-6-tooth" />
+    <template #content>
+      <div class="p-3 min-w-30 sm:min-w-40 md:min-w-60 space-y-4">
+        <UFormField label="Image Type">
+          <URadioGroup v-model="imageType" :items="imageOptions" />
+        </UFormField>
+        <UFormField
+          label="Quality" :ui="{
+            container: 'flex flex-col md:flex-row md:items-center mt-0 md:mt-1',
+          }"
+        >
+          <span class="mb-1 md:mb-0 md:mr-2">{{ (quality * 100).toFixed(0) }}%</span>
+          <USlider v-model="quality" :min="0" :max="1" :step="0.01" />
+        </UFormField>
+      </div>
+    </template>
+  </UPopover>
 </template>
